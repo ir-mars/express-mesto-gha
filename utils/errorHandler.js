2df8c3142e057eb26fa263dd34608ee858acef24
Irina Demina <ir-mars@yandex.ru>
2023-06-29 15:48:59 +0600

    сборка проекта

---
+++ utils/errorHandler.js
@@ -0,0 +1,33 @@
+const ERROR_INTERNAL_SERVER = 500;
+const ERROR_NOT_FOUND = 404;
+const ERROR_BAD_REQUEST = 400;
+
+class NotFoundError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "NotFoundError";
+  }
+}
+
+function errorHandler (error, response) {
+  const { name } = error;
+
+  if (name === 'CastError' || name === 'ValidationError' || name === 'OtherError') {
+    return response.status(ERROR_BAD_REQUEST).send({ message: 'Переданы некорректные данные' });
+  } else if (name === 'NotFoundError') {
+    return response.status(ERROR_NOT_FOUND).send({ message: 'Данные не найдены' });
+  } else {
+    return response.status(ERROR_INTERNAL_SERVER).send({ message: 'Произошла ошибка сервера ' + ERROR_INTERNAL_SERVER });
+  }
+}
+
+function notFoundErrorThrow () {
+  throw new NotFoundError();
+}
+module.exports = {
+  ERROR_INTERNAL_SERVER,
+  ERROR_NOT_FOUND,
+  ERROR_BAD_REQUEST,
+  errorHandler,
+  notFoundErrorThrow,
+};
