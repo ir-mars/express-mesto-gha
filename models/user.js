72cbd5079c57d0845dd5e674f39e521365680f65
Irina Demina <ir-mars@yandex.ru>
2023-06-30 16:10:27 +0600

    добавить в схему пользователя валидацию

--- models/user.js
+++ models/user.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const { URL_REGEXP } = require('../utils/constants');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -16,18 +19,54 @@ const userSchema = new mongoose.Schema({
   },
   avatar: {
     type: String,
+    validate: {
+      validator: function (url) {
+        return URL_REGEXP.test(url);
+      },
+      message: function ({ value }) {
+        return value + 'неверный URL';
+      },
+    },
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
   email: {
     type:String,
     required: true,
-    unique: true
+    unique: true,
+    validate: {
+      validator: function (email) {
+        return validator.isEmail(email);
+      },
+      message: function ({ value }) {
+        return `Ошибка! ${value} не является электронной почтой`;
+      },      
+    },
   },
   password: {
     type: String,
     required: true,
     select: false,
   },
-  });
+},
+{
+  toObject: { useProjection: true},
+  toJSON: { useProjection: true},
+});
+
+userSchema.statics.findUserByCredentials = async function (email, password) {
+  try {
+    const user = await this.findOne({ email }).select('+password');
+    if (!user) {
+      throw new Error('Неправильные почта или пароль');
+    }
+    const isMatched = await bcrypt.compare(password, user.password);
+    if (!isMatched) {
+      throw new Error('Неправильные почта или пароль');
+    }
+    return user;
+  } catch (error) {
+    throw error;
+  }
+};
 
 module.exports = mongoose.model('user', userSchema);
---
+++ utils/constants.js
@@ -0,0 +1,7 @@
+const URL_REGEXP = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
+const JWT_CODE = '5NjYsImV4cCI6MTY4MzAyMzc2Nn0eyJfaWQiOiI2NDQ3OGJkZGRhNzVlYWYxZjZlNDU3YWEiLCJpYXQiOjE2ODI0MTg';
+
+module.exports = {
+  JWT_CODE,
+  URL_REGEXP
+};
